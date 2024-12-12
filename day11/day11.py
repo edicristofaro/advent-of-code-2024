22f34ea74c71cbb815af60a62f112741727f2ca8
Ed Di Cristofaro <edicristofaro@gmail.com>
2024-12-11 18:21:19 -0800

    Day 11 complete

--- day11/day11.py
+++ day11/day11.py
@@ -18,23 +18,44 @@ def part1(stones, blinks):
             else:
                 new_stones.append(str(int(s) * 2024))
         stones = new_stones
-        print(i + 1, len(stones))
 
     return len(new_stones)
 
 
 def part2(stones, blinks):
     # Brute force isn't going to work, but let's try it anyway.
-    return part1(stones, blinks)  # Nope!
+    # return part1(stones, blinks)  # Nope!
+    stone_counts = {}
+    for s in stones:
+        stone_counts[s] = 1
+
+    for b in range(1, blinks + 1):
+        new_counts = {}
+        for k, v in stone_counts.items():
+            # handle 0s
+            if k == "0":
+                new_counts["1"] = new_counts.get("1", 0) + v
+            # handle numbers of even length
+            elif len(k) % 2 == 0:
+                left = k[0 : int(len(k) / 2)]
+                right = k[int(len(k) / 2) :]
+                new_counts[str(int(left))] = new_counts.get(str(int(left)), 0) + v
+                new_counts[str(int(right))] = new_counts.get(str(int(right)), 0) + v
+            else:
+                new_counts[str(int(k) * 2024)] = (
+                    new_counts.get(str(int(k) * 2024), 0) + v
+                )
+        stone_counts = new_counts
+
+    return sum([v for v in stone_counts.values()])
 
 
 def main():
     # Read input
-    with open("sample.txt", "r") as f:
+    with open("input.txt", "r") as f:
         input_data = f.read()
 
     stones = input_data.split()
-    print(stones)
 
     # Solve and print results
     print("Part 1:", part1(stones, 25))
